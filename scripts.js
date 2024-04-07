a348668c0034602f5c5a835bd149691d436e56d8
rodrigopinesso <142932782+rodrigopinesso@users.noreply.github.com>
2024-04-06 23:13:47 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,31 +1,15 @@
-function getQueryStringValue(key) {
-    return decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
-  }
-  
-  function loadPokemon(evolutionName) {
-    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${evolutionName}`;
-    
-    fetch(apiUrl)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        const pokemonImage = document.createElement('img');
-        pokemonImage.src = data.sprites.front_default;
-        pokemonImage.alt = `${data.name} image`;
-        pokemonImage.setAttribute('aria-label', `${data.name} image`);
-        
-        document.body.appendChild(pokemonImage);
-      })
-      .catch(error => {
-        console.log('There was a problem with the fetch operation:', error.message);
-      });
-  }
-  
-  const evolutionName = getQueryStringValue('nome_da_evolucao');
-  
-  loadPokemon(evolutionName);
-  
+var link = location.search;
+
+var t = new URLSearchParams(link);
+
+var r = t.get("evolucao");
+
+document.title = "Página do " + r;
+
+fetch("https://pokeapi.co/api/v2/pokemon/wartortle")
+.then(data => {
+    return data.json();
+})
+.then(post => {
+    console.log(post.title);
+})
